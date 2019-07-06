cda88ceb16bf7f9b4de2a17e748e136f3bd80677
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2019-07-05 21:46:12 -0300

    Validação dos dados do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,28 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .required()
+        .min(3),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    try {
+      await schema.validate(req.body);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    if (await User.findOne({ where: { email: req.body.email } })) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
     const { id, name, email } = await User.create(req.body);
 
     return res.json({
